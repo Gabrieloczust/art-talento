922752236aa74a781f65531201d67bc2bc38f41c
Gabriel Oczust <gabriel.oczust@gmail.com>
2020-11-22 12:00:33 -0300

    Constants Scss e Px -> Rem

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,31 +1,8 @@
 import styles from './Header.module.scss'
-// import { FaWhatsapp } from 'react-icons/fa'
-// import { IoIosMail } from 'react-icons/io'
-// import { SiGooglemaps } from 'react-icons/si'
-
-// const whatsAppColor = "#25D366";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            {/* <div className={styles.supmenu}>
-                <a className={styles.contactIcon} href="https://goo.gl/maps/5UHYLPQX3WD48vfb9" target="_blank">
-                    <SiGooglemaps/>
-                    <span>Rua Lizimaco Serapião, 64 - Novo Mundo</span>
-                </a>
-                <div className={styles.contact}>
-                    <a className={styles.contactIcon} href="mailto:anderson@arttalento.com.br">
-                        <IoIosMail />
-                        <span>anderson@arttalento.com.br</span>
-                    </a>
-                    <div className={styles.separator}></div>
-                    <a className={styles.contactIcon} href="http://wa.me/554133878068" target="_blank">
-                        <FaWhatsapp color={whatsAppColor}/>
-                        <span><b>41</b> 3387-8068</span>
-                    </a>
-                </div>
-            </div> */}
-
             <div className={styles.menu}>
                 <span className={styles.logo}>Art&Talento</span>
 
@@ -37,7 +14,6 @@ export default function Header() {
                     <a>Contatos</a>
                 </nav>
             </div>
-
         </header>
     )
 }
