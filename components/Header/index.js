6b0d08a80e2204cf47456f0a17fe4ae5fb35cc15
Gabriel Oczust <gabriel.oczust@gmail.com>
2020-11-22 20:19:04 -0300

    Header - Mobile

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import styles from './Header.module.scss'
+import { BiMenu } from 'react-icons/bi'
 
 export default function Header() {
+
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const changeMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.menu}>
                 <span className={styles.logo}>Art&Talento</span>
 
-                <nav className={styles.nav}>
+                <div id={styles.hambuguer} onClick={changeMenu}>
+                    <BiMenu />
+                </div>
+
+                <nav className={styles.nav} data-open={openMenu ? 'true' : 'false'} onClick={() => setOpenMenu(false)}>
                     <a className={styles.active}>Home</a>
                     <a>Quem Somos</a>
                     <a>Serviços</a>
